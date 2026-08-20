136fa3457bc155d1922a8ea443ae5040d6cb566b
Nikos Gkorogiannis <nikos.gorogiannis@gmail.com>
2026-08-20 17:23:08 +0200

    Fix discrepancy in json fields between 5.0 and 5.3 implementations
    gc-stats for 5.3 uses `stats_reliable` where 5.0 uses `stats-reliable`. As the other fields generally are in snake case, fix the 5.0 version.

--- lib/olly_gc_stats/olly_gc_stats.5.0.ml
+++ lib/olly_gc_stats/olly_gc_stats.5.0.ml
@@ -110,7 +110,7 @@ let print_percentiles json output hist outliers =
     "forced_major": %i,       
     "compactions": %i
   },       
-  "stats-reliable": %b
+  "stats_reliable": %b
 }|}
       real_time !total_cpu_time total_gc_time gc_overhead
       (Olly_common.Process_poller.peak_rss ())
